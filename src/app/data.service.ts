27a1f2f73bca273278eae2e4dad74dbcbe13b730
fabioir <fabioiglesias@hotmail.es>
2018-07-26 15:42:39 +0200

    Revert "Trouble with toXML"
    This reverts commit cf3235a4ca7d2f22e1a4301bd18e4a9771cca15b.

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Definitions, DecisionTable } from './metamodel-classes/metamodelClasses';
 import { Observable, Subject } from 'rxjs';
-import { DmnService } from './dmn.service';
 
 @Injectable({
   providedIn: 'root'
@@ -16,12 +15,8 @@ export class DataService {
   dmnUpdate = new Subject();
   tableUpdate = new Subject();
 
-  constructor(private dmnService: DmnService) { }
+  constructor() { }
 
-  /**
-   * Sets the .dmn document
-   * @param xml new .dmn document
-   */
   setXML(xml: string) {
     if (xml) {
       this.xml = xml
@@ -31,10 +26,6 @@ export class DataService {
     }
   }
 
-  /**
-   * Sets the DMN Moddle object
-   * @param dmn object created with the Moddle
-   */
   setDMN(dmn: Definitions) {
     console.log("setting dmn in data service");
     if (dmn) {
@@ -45,39 +36,23 @@ export class DataService {
     }
   }
 
-  /**
-   * Sets the Decision table
-   * @param table Object DecisionTable
-   */
   setTable(table: DecisionTable) {
     if (table) {
       this.table = table;
-      this.table.checkId();
       this.tableUpdate.next(this.table);
-      //this.dmnService.toDMN(this.table);
-
     } else {
       console.log("Decision Table problem");
     }
   }
 
-  /**
-   * Returns an observable with the .dmn document updates
-   */
   getXMLUpdates(): Observable<string> {
     return <Observable<string>>this.xmlUpdate.asObservable();
   }
 
-  /**
-   * Returns an observable with the DMN Moddle object updates
-   */
   getDMNUpdates(): Observable<Definitions> {
     return <Observable<Definitions>>this.dmnUpdate.asObservable();
   }
 
-  /**
-   * Returns an observable with the Decision Table updates
-   */
   getTableUpdates(): Observable<DecisionTable> {
     return <Observable<DecisionTable>>this.tableUpdate.asObservable();
   }
